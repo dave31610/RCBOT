cb95f6fd8872c68a119ac46e2ecfb49dd7e0a229
dave van den berg <57539200+dave31610@users.noreply.github.com>
2021-02-08 21:39:14 +0100

    Init
    init

---
+++ commands/doei.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+return message.channel.send("ik dacht dat je de alles kunner bent nou tot nooit meer");
+
+}
+
+module.exports.help = {
+    name: "doei"
+}
---
+++ commands/info.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    var botEmbed = new discord.MessageEmbed()
+            .setTitle('luister naar ons radio zender')
+            .setDescription("klik hier http://www.radio-cornda.xyz")
+            .setColor("#0099ff")
+            .addField("Bot naam", client.user.username)            
+            .setTimestamp()
+            .setFooter('veel luister plezier');
+
+        return message.channel.send(botEmbed);
+
+}
+
+module.exports.help = {
+    name: "info"
+}
