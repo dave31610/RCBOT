01730a7ec77095674eacbdfc00aeb08b6fbf6e25
dave van den berg <57539200+dave31610@users.noreply.github.com>
2021-02-09 23:33:07 +0100

    Init
    Init

--- commands/level.js
+++ commands/level.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js")
 const fs = require("fs");
-const canvaCord = require("canvarcord");
+const canvacord = require("canvacord");
 
 module.exports.run = async (client, message, args) => {
 
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 
     if (levelFile[member]) {
 
-        const rank = new canvaCord.Rank()
+        const rank = new canvacord.Rank()
         .setAvatar(message.author.displayAvatarURL({dynamic: false, format: 'png'}))
         .setCurrentXP(levelFile[member].xp)
         .setLevel(levelFile[member].level)
