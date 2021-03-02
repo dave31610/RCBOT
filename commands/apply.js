541bb7490cf0be2232f83a50aaa3105442a338d7
dave van den berg <57539200+dave31610@users.noreply.github.com>
2021-03-02 17:56:22 +0100

    Init
    Init

--- commands/apply.js
+++ commands/apply.js
@@ -137,10 +137,10 @@ module.exports.run = async (client, message, args) => {
                                                 var antwoord8 = antwoord.first();;
 
                                                 var uitkomst = new discord.MessageEmbed()
-                                                .setTitle("Bedankt voor het Solliciteren")
-                                                .setColor("00ff00")
-                                                .setTimestamp()
-                                                .SetDescription(`**Vraag 1:** \n${antwoord1}\n\n**Vraag 2:** \n${antwoord2}\n\n**Vraag 3:** \n${antwoord3}\n\n**Vraag 4:** \n${antwoord4}\n\n**Vraag 5:** \n${antwoord5}\n\n**Vraag 6:** \n${antwoord6}\n\n**Vraag 7:** \n${antwoord7}\n\n**Vraag 8:** \n${antwoord8}`);
+                                                    .setTitle("Bedankt voor het Solliciteren")
+                                                    .setColor("#00ff00")
+                                                    .setTimestamp()
+                                                    .SetDescription(`**Vraag 1:** \n${antwoord1}\n\n**Vraag 2:** \n${antwoord2}\n\n**Vraag 3:** \n${antwoord3}\n\n**Vraag 4:** \n${antwoord4}\n\n**Vraag 5:** \n${antwoord5}\n\n**Vraag 6:** \n${antwoord6}\n\n**Vraag 7:** \n${antwoord7}\n\n**Vraag 8:** \n${antwoord8}`);
                                             
                                                 settedParent.bulkDelete(16).then(
                                                     settedParent.send(uitkomst)
